4fdd71d9bc269464e10201d0f696bb4ab8513927
rhit-lopezmo <78335296+rhit-lopezmo@users.noreply.github.com>
2024-01-10 21:42:57 -0500

    clean up js files and scripts refs in html

--- public/scripts/level-select.js
+++ public/scripts/level-select.js
@@ -1,36 +1,7 @@
-/**
- * @fileoverview
- * Provides the JavaScript interactions for all pages.
- *
- * @author 
- * PUT_YOUR_NAME_HERE
- */
-
-/** namespace. */
 var rhit = rhit || {};
 
-/** globals */
-rhit.variableName = "";
-
-/** function and class syntax examples */
-rhit.functionName = function () {
-	/** function body */
-};
-
-rhit.ClassName = class {
-	constructor() {
-
-	}
-
-	methodName() {
-
-	}
-}
-
-/* Main */
-/** function and class syntax examples */
 rhit.main = function () {
-	console.log("Ready");
+	console.log("Level Select Ready");
 };
 
 rhit.main();
