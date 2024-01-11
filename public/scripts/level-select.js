d8dcabd0a3db4be414770ea251bf82cd9cc30ac6
rhit-lopezmo <78335296+rhit-lopezmo@users.noreply.github.com>
2024-01-10 21:53:15 -0500

    finished js for level select

--- public/scripts/level-select.js
+++ public/scripts/level-select.js
@@ -2,6 +2,18 @@ var rhit = rhit || {};
 
 rhit.main = function () {
 	console.log("Level Select Ready");
+
+	// get all the level buttons
+	const levelButtons = document.querySelectorAll(".level-button")
+
+	console.log(levelButtons);
+
+	// create listener for buttons
+	for (const level of levelButtons) {
+		level.onclick = () => {
+			document.location = './gameplay.html';
+		}
+	}
 };
 
 rhit.main();
